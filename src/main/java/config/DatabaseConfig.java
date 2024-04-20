e2973ad4230448555177db50d6a068b984b6145a
Eduardo Delgado <jedcanchola@gmail.com>
2024-04-20 15:12:56 -0600

    Push remove sensitive data

---
+++ src/main/java/config/DatabaseConfig.java
@@ -0,0 +1,23 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class DatabaseConfig {
+
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  @Value("${spring.datasource.username}")
+  private String dbUsername;
+
+  @Value("${spring.datasource.password}")
+  private String dbPassword;
+
+}
